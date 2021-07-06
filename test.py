c6a3bb8af34845019b5affb86d0cd3a2c0f8610e
Donna DeMarco <donna.demarco@seo-usa.org>
2021-07-06 18:24:24 +0000

    phase1 v1

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ class TestBadStyle(unittest.TestCase):
         self.assertEqual(dec(1), 0)
         self.assertEqual(dec(0), -1)
         self.assertEqual(dec(2.1), 1.1)
-    
+
     def test_add(self):
-        self.assertEqual(add(2,1), 3)
+        self.assertEqual(add(2, 1), 3)
         self.assertEqual(add(2.1, 1.2), 3.3)
-    
-    
+        self.assertEqual(add(2, 1), 4)
+
+
 if __name__ == '__main__':
     unittest.main()
